86cfc582729162c829901f2e90ce8ea9897b9852
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2023-02-26 15:42:20 +0200

    update Portfolio to new Version

---
+++ render_files.js
@@ -0,0 +1,14 @@
+// Request and cache critical resources
+const resources = ['/index.html', '/css/main.css',  '/dist/bundle.js']; // '/dist/bundle.js'
+caches.open('critical-resources')
+    .then(cache => cache.addAll(resources));
+  
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = '/css/main.css';
+document.head.appendChild(link);
+const script = document.createElement('script');
+script.src = '/dist/bundle.js';
+document.body.appendChild(script);
+
