e9bc15afd30d4bca2033172cf5fd508d4c192a9f
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2023-03-01 14:06:35 +0200

    add cacheing img

--- render_files.js
+++ render_files.js
@@ -12,3 +12,21 @@ const script = document.createElement('script');
 script.src = '/dist/bundle.js';
 document.body.appendChild(script);
 
+const imgElements = document.querySelectorAll('img');
+
+imgElements.forEach(img => {
+  img.addEventListener('load', () => {
+    const imgSrc = img.getAttribute('src');
+    const cacheTime = 604800; // 1 week in seconds
+    fetch(imgSrc, { cache: 'no-store' })
+      .then(response => {
+        const headers = new Headers(response.headers);
+        headers.set('Cache-Control', `public, max-age=${cacheTime}`);
+        const newResponse = new Response(response.body, { headers });
+        caches.open('my-cache')
+          .then(cache => {
+            cache.put(imgSrc, newResponse);
+          });
+      });
+  });
+});
