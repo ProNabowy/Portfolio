278d93e0610f0054d521d721cd93fea9e2a8d5aa
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2023-03-06 12:03:24 +0200

    Update render_files.js

--- render_files.js
+++ render_files.js
@@ -1,12 +1,12 @@
 // Request and cache critical resources
-const resources = ['/index.html', '/css/main.css',  '/dist/bundle.js']; // '/dist/bundle.js'
+const resources = ['/index.html', '/dist/bundle.css',  '/dist/bundle.js']; // '/dist/bundle.js'
 caches.open('critical-resources')
     .then(cache => cache.addAll(resources));
   
 
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = '/css/main.css';
+link.href = '/dist/bundle.css';
 document.head.appendChild(link);
 const script = document.createElement('script');
 script.src = '/dist/bundle.js';
