e29944f4dbe0dd5818408e6b9449575c5d0f0c03
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2023-03-06 12:28:00 +0200

    Update render_files.js

--- render_files.js
+++ render_files.js
@@ -4,10 +4,6 @@ caches.open('critical-resources')
     .then(cache => cache.addAll(resources));
   
 
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = '/dist/bundle.css';
-document.head.prepend(link);
 const script = document.createElement('script');
 script.src = '/dist/bundle.js';
 document.body.appendChild(script);
